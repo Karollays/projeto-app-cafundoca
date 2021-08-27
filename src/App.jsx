b3d87cd4238c417f360e1c1137b2abe30d721cc8
bianca-scharneski <biancascharneski@gmail.com>
2021-08-27 00:20:29 -0300

    subindo button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Button from './Components/Button/Button';
 
 function App() {
   return (
     <p>
-      Cleanedup React App
+      <Button/>
     </p>
   );
 }
