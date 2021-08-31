2706d9041af1b5126b1146b03629c6c7ad5bc79f
nithack <nithack@DESKTOP-54R9MGP.localdomain>
2021-08-31 13:31:10 -0300

    feat:background opacit

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ import Teste from './components/Teste/Teste';
 import Home from './components/Home/Home'
 function App() {
   return (
-    <div className="container">
-      <BrowserRouter>
+    <div className="body-edit ">
+      <BrowserRouter className="container">
         <Header />
         <Menu />
         <div className="article">
