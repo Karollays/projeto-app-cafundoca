1b47b693f9b6fac279fa0714f9e14c831b10f2b6
Karollays <karol.penteadoalves@gmail.com>
2021-08-31 01:10:09 -0300

    factor: nova branche

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Header from './components/Header/Header.jsx';
 import Article from './components/Article/Article';
 import Footer from './components/Footer/Footer';
+import './App.css';
 
 function App() {
 
   return (
-    <div>
+    <div className="container">
       <Header />
       <Article />
       <Footer />
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,5 +1,6 @@
 import React from 'react'; 
 import Button from '../Button/Button';
+import './Menu.css'
 
 function Menu() {
      return (
