03238f03d708c1a0dd101adec069266bb590a708
Karollays <karol.penteadoalves@gmail.com>
2021-08-31 00:41:04 -0300

    refact: menu e button

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,9 +1,21 @@
 import React from 'react'; 
-
+import Button from '../Button/Button';
 
 function Menu() {
      return (
-     <h1>Hello World</h1> 
+         <div className="menu">
+     <ul>
+         <Button texto="Login" ></Button>
+         <Button texto="Compras" ></Button>
+         <Button texto="Clientes" ></Button>
+         <Button texto="Produtos" ></Button>
+         <Button texto="Fornecedores" ></Button>
+         <Button texto="Pedidos" ></Button>
+     
+     </ul>
+     </div>
+
+     
      );
 }
 export default Menu;
