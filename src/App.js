67d9489cd784175f7093d0bc1a0c60a739c5c102
Karollays <karol.penteadoalves@gmail.com>
2021-08-30 23:38:25 -0300

    feat:subindo arquivos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-
-function App() {
-  return (
-    <div>
-      Olá, Karol!
-    </div>
-  );
+import Header from './components/Header/Header.jsx';
+import Article from './components/article/Article';
+import Footer from './components/footer/Footer';
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Article />
+        <Footer />
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/components/article/Article.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Article extends React.Component{
+    render(){
+        return(
+            <div>
+            <h3>Título do artigo</h3>
+            <p>Conteúdo do artigo</p>
+            </div>
+        );
+    }
+}
+
+export default Article;
