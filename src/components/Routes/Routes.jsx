526ddfd1ffe4bf63b03f0755c55005c889b123dd
nithack <nithack@DESKTOP-54R9MGP.localdomain>
2021-09-01 00:20:17 -0300

    refactor: Rotes and App

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -1,16 +1,30 @@
 import React from "react";
-import { Route, BrowserRouter } from "react-router-dom";
+import { Route, BrowserRouter } from "react-router-dom"
 
-import Login from "./../Login/Login";
-import Teste from "./../Teste/Teste"
+import Header from './../Header/Header.jsx';
+import Home from './../Home/Home';
+import Compras from './../Compras/Compras';
+import Clientes from './../Clientes/Clientes';
+import Produtos from './../Produtos/Produtos';
+import Fornecedores from './../Fornecedores/Fornecedores';
+import Pedidos from './../Pedidos/Pedidos';
+import Container from './../Container/Container.jsx';
+import Footer from './../Footer/Footer';
 
 const Routes = () => {
    return(
-       <BrowserRouter>
-           <Route component = { Login }  path="/" exact />
-           <Route component = { Teste }  path="/teste" exact />
-           
-       </BrowserRouter>
+    <BrowserRouter>
+    <Header />
+    <Container>
+    <Route component={Home} path="/home" exact/>
+    <Route component={Compras} path="/compras" />
+    <Route component={Clientes} path="/clientes" />
+    <Route component={Produtos} path="/produtos" />
+    <Route component={Fornecedores} path="/fornecedores" />
+    <Route component={Pedidos} path="/Pedidos" />
+    </Container>
+    <Footer />
+  </BrowserRouter>
    )
 }
 
