f749e7e865f9c911702c93136625d350e2fae32f
nithack <nithack@DESKTOP-54R9MGP.localdomain>
2021-09-01 00:22:08 -0300

    feat add login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
-
+import InputField from '../Input/InputField';
+import { Link } from 'react-router-dom';
+import Button from './../Button/Button';
+import './Login.css'
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="container-login">
+      <div className="login-form">
+        <div className="login-form-image">
+          <img src="https://www.w3schools.com/howto/img_avatar.png" style={{ borderRadius: '50%'}} alt="avatar" />
+        </div>
+        <div className="login-form-input">
+          <span className="login-form-title">Login</span>
+          <InputField text="Email" width="100%" height="50px" placeholder="Email"/>
+          <InputField text="Password"  width="100%" height="50px" placeholder="Password"/>
+          <Link to="/home" className="btn"><Button className="login-form-button" texto="Login"></Button></Link>
+          <a href={'localhost:3000'}>Forgot password? </a>
+          <a href={'localhost:3000'}>Register</a>
+        </div>
+      </div>
     </div>
   );
 }
