1d3f5bd7e9782091c99ca609cabf1da06603f8d4
Andrey H. Nithack <59296988+Nithack@users.noreply.github.com>
2021-09-01 01:27:02 -0300

    Revert "Dev matheus"

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import "./Footer.css";
-import { GrLinkedin } from 'react-icons/gr';
-import { FiInstagram } from 'react-icons/fi';
-import { FaFacebookSquare } from 'react-icons/fa'
 
 function Footer() {
     return (
         <div className="footer">
-            <h4>&copy; Since 2018</h4>
-            < GrLinkedin size= {35} style={{ margin: 8 }}/> 
-            < FiInstagram size = {35} style={{ margin: 8 }}/>
-            < FaFacebookSquare size= {35} style={{ margin: 8 }}/>
+            <h4>Footer</h4>
         </div>
     );
 
