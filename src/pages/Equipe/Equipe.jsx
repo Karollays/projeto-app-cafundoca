a414a013a9da27dd9b872134692d0eed93d2098d
Karollays <karol.penteadoalves@gmail.com>
2021-09-02 00:51:24 -0300

    feat: equipe sobre

--- src/pages/Equipe/Equipe.jsx
+++ src/pages/Equipe/Equipe.jsx
@@ -5,6 +5,17 @@ import Header from '../../components/Header/Header';
 import ImagemEquipe from '../../components/ImagemEquipe/ImagemEquipe';
 import andreyImg from './../../rest/andrey.png';
 import andreyMeme from './../../rest/memeAndrey.jpg';
+import biancaImg from './../../rest/bianca.png';
+import biancaMeme from './../../rest/memeBianca.jpeg';
+import camilaImg from './../../rest/camila.png';
+import camilaMeme from './../../rest/memeCamila.jpeg';
+import matheusImg from './../../rest/matheus.png';
+import matheusMeme from './../../rest/memeMatheus.png';
+import karolineImg from './../../rest/karol.png';
+import karolineMeme from './../../rest/memeKarol.jpeg';
+import imgLink from './../../rest/linkedin.png';
+import imgGit from './../../rest/github.png';
+
 import './Equipe.css';
 export default function Equipe() {
   return (
@@ -12,9 +23,42 @@ export default function Equipe() {
       <Header />
       <Container>
         <ImagemEquipe
+
+          
           name="Andrey Nithack"
           imgFirst={andreyImg}
           imgMeme={andreyMeme}
+          imgLink={imgLink}
+          imgGit={imgGit}
+         
+          />
+           <ImagemEquipe
+          name="Bianca Scharneski"
+          imgFirst={biancaImg}
+          imgMeme={biancaMeme}
+          imgLink={imgLink}
+          imgGit={imgGit}
+          />
+          <ImagemEquipe
+          name="Camila Trizotto"
+          imgFirst={camilaImg}
+          imgMeme={camilaMeme}
+          imgLink={imgLink}
+          imgGit={imgGit}
+          />
+          <ImagemEquipe
+          name="Matheus Couto"
+          imgFirst={matheusImg}
+          imgMeme={matheusMeme}
+          imgLink={imgLink}
+          imgGit={imgGit}
+          />
+          <ImagemEquipe
+          name="Karol Penteado"
+          imgFirst={karolineImg}
+          imgMeme={karolineMeme}
+          imgLink={imgLink}
+          imgGit={imgGit}
         />
       </Container>
       <Footer />
